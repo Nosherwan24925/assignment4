6177a7c03db459f98d3a3a6ae9c011a8dfee6380
Nosherwan <94525538+Nosherwan24925@users.noreply.github.com>
2024-02-09 18:25:12 +0500

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+class Calculator
+{
+
+private:
+
+float  num1;
+float num2;
+
+public:
+
+Calculator(float n1,float n2);
+
+void display_sum();
+void display_subtration();
+void display_multiply();
+
+};
+
+
+
+
+int main()
+{
+return 0;
+}
